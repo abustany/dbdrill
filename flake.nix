01464d4396f65f9df749b2fa88250a5177e22375
Adrien Bustany <adrien@rapmed.net>
2025-09-26 10:06:58 +0200

    Set up crane build

--- flake.nix
+++ flake.nix
@@ -11,9 +11,10 @@
       };
     };
     flake-utils.url  = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -21,9 +22,23 @@
           inherit system overlays;
         };
         rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+
+        craneCommonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          buildInputs = [] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [];
+        };
+
+        dbdrill = craneLib.buildPackage(
+          craneCommonArgs // { cargoArtifacts = craneLib.buildDepsOnly craneCommonArgs; }
+        );
       in
       with pkgs;
       {
+        checks = { inherit dbdrill; };
+        packages.default = dbdrill;
+        apps.default = flake-utils.lib.mkApp { drv = dbdrill; };
         devShells.default = mkShell {
           buildInputs = [
             rust
